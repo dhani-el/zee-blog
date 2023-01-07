48f2c1bb1d9a5b713afb6abf774d6b66f56391d1
omotayo daniel ukuhor <ukuhoromotayo@gmail.com>
2023-01-07 19:42:54 +0100

    updated data to be sent for blogs end point

--- Server/Utils/blogUtils.js
+++ Server/Utils/blogUtils.js
@@ -1,7 +1,7 @@
 
 
 async function getBlogs(DB , startIndex , limit = 5){
-    const data = await DB.find().skip(startIndex * limit).limit(limit).select("title").select("genre").select("readTime" ).select("image").select("isPaid") ;
+    const data = await DB.find().skip(startIndex * limit).limit(limit).select("title").select("genre").select("readTime" ).select("image").select("isPaid").select("body") ;
     return data;
 }
 
