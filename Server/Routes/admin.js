f4677ddd28bf06b92289f2551ce4f229e856a6b7
omotayo daniel ukuhor <ukuhoromotayo@gmail.com>
2023-01-10 17:30:44 +0100

    cors issue

--- Server/Routes/admin.js
+++ Server/Routes/admin.js
@@ -1,8 +1,12 @@
 const express  = require("express")
 const router  = express.Router();         
 const BLOG_DB = require("../Schemas/blogSchema");
+const cors = requuire("cors");
 const{upload , saveImageToS3 , randomBytes} = require("../Utils/adminUtils");
 
+router.use(cors({
+    origin:"http://localhost:3000/admin/post"
+        }));
 router.use(express.json());
 router.use(express.urlencoded({extended:false}));
 
