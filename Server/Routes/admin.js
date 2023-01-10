b6e6f08e65eede3b65636a2fd35281ecdc87e79b
omotayo daniel ukuhor <ukuhoromotayo@gmail.com>
2023-01-10 19:52:43 +0100

    added code to stop server crashing

--- Server/Routes/admin.js
+++ Server/Routes/admin.js
@@ -10,12 +10,14 @@ const{upload , saveImageToS3 , randomBytes} = require("../Utils/adminUtils");
 router.post("/post", upload.single("image") ,async function(req,res){
     console.log("this is req . body",req.body);
     // console.log("this is req . files",req.file);
-    const imageName = req.file.buffer ? await randomBytes() :  "no Image"
+    const imageName = await randomBytes();
 
-    if(req.file.buffer){
+    if(!req.file.buffer){
+        console.log("no buffer");
+    return res.send("no buffer")
+}
     await saveImageToS3(imageName , req.file.buffer , req.file.mimetype);
     console.log("this is the image name",imageName);
-}
     console.log("req.body is " , req.body);
     await BLOG_DB.create({...req.body , image: imageName});
     res.send("post successful");
