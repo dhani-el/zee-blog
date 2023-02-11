d75e847ce2aad53d89e1ebce59c13c26c918b385
omotayo daniel ukuhor <ukuhoromotayo@gmail.com>
2023-02-12 00:19:09 +0100

    date implementation

--- Server/Schemas/blogSchema.js
+++ Server/Schemas/blogSchema.js
@@ -1,6 +1,7 @@
 const mongoose  = require('mongoose');
 
 
+
 const BlogSchema = new mongoose.Schema({
     title:{
         type : String,
@@ -21,6 +22,7 @@ const BlogSchema = new mongoose.Schema({
     },
     date:{
         type: String,
+        default: new Date().toLocaleDateString()
     },
     image:{
         type: String,
