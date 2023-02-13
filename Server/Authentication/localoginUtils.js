a7cef6394e14e4716337df788d5a26226a9d5028
omotayo daniel ukuhor <ukuhoromotayo@gmail.com>
2023-02-13 12:30:40 +0100

    enabled some logs to track errors

--- Server/Authentication/localoginUtils.js
+++ Server/Authentication/localoginUtils.js
@@ -16,19 +16,20 @@ module.exports = passport.use(new Strategy(async function _verify(username,passw
             return done(null, false);
     }
     const user = await DB.find().where("name").equals(username).select("name").select("email");
+    console.log("user is currently " , user);
     return done(null , user);
 }));
 
 
 passport.serializeUser(function(user , done){
-    // console.log("inside serializer this is the user",user);
+    console.log("inside serializer this is the user",user);
     process.nextTick(function(){
        return done(null , {...user})
     });
 })
 
 passport.deserializeUser(function(user , done){
-    // console.log("inside deserializer this is the user",user);
+    console.log("inside deserializer this is the user",user);
     process.nextTick(function(){
        return done(null , {...user})
     });
@@ -43,6 +44,7 @@ function aFieldIsEmpty(username , password){
 }
 
 async function userDoesNotExist(username){
+    console.log("username passsed is" , username);
     const data = await DB.find().where("name").equals(username).select("name");
     if (data.length == 0  ){
         return true;
@@ -51,6 +53,7 @@ async function userDoesNotExist(username){
 }
 
 async function passwordIsIncorrect(password , username){
+    console.log(password);
     const hashed = await DB.find().where("name").equals(username).select("password");
    return !(await bcrypt.compare(password ,hashed[0].password ))
 }
