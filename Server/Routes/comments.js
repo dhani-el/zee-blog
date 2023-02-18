56c0edd446bf2de3861d985fd8ec3aa90678d112
omotayo daniel ukuhor <ukuhoromotayo@gmail.com>
2023-02-18 16:07:28 +0100

    ihphp

--- Server/Routes/comments.js
+++ Server/Routes/comments.js
@@ -6,22 +6,23 @@ const {getComments,uploadComment} = require("../Utils/commentUtils");
 
 
 router.get("/:title/:index" , async function(req, res){
-    res.json(await getComments(DB , index , req.params.title));
+    res.json(await getComments(DB , startIndex , req.params.title));
 });
 
 router.post("/post" , async function(req, res){
     if(req.user === undefined ){
-        return res.send(" you need to be logged in to comment ")
+        const data = {
+            title: req.body.title,
+            username: req.user.name,
+            body: req.body.comment
+        }
+        console.log(req.body);
+        console.log(req.user["0"].name);
+        await uploadComment(DB , data)
+        res.send("comment successfully sent ");
     }
-    const data = {
-        title: req.body.title,
-        username: req.user.name,
-        body: req.body.comment
-    }
-    console.log(req.body);
-    console.log(req.user);
-    await uploadComment(DB , data)
-    res.send("comment successfully sent ");
+    return res.send(" you need to be logged in to comment ")
+
 });
 
 
